2114ce4847c959055ea00906c24850b331e805ec
chadl76 <chad.lucas.19@gmail.com>
2018-01-09 14:48:47 -0800

    add styling to post edit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,6 +8,29 @@ class PostsController < ApplicationController
 		@post = Post.new
 	end
 
+	def edit 
+		@post = Post.find(params[:id])
+	end
+
+	def show
+		@post = Post.find(params[:id])
+	end
+
+	def update
+		@post = Post.find(params[:id])
+		@post.update(post_params)
+		
+			redirect_to post_path(@post)
+	
+	end
+
+	def destroy
+		@post = Post.find(params[:id])
+
+		@post.destroy
+		redirect_to :root
+	end
+
 
 	def create
 		@post = Post.create(post_params)
