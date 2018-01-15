388006e4a97875c3b9f12897fb22c671c9257b60
chadl76 <chad.lucas.19@gmail.com>
2018-01-15 15:09:50 -0800

    1

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,8 +4,6 @@ class Post < ApplicationRecord
 	#validates :user_id, presence: true
 	has_attached_file :image, styles: { :medium => "640x"}
 	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
-	has_attached_file :avatar, styles: {:medium => "152x152#"}
-	validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 	belongs_to :user
 	has_many :comments
 end
